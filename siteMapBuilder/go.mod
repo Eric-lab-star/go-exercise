10a4102a86e78f962d27d3c216f538f6d7a366f0
eric-lab-star <cyon256@icloud.com>
2023-01-18 19:47:28 +0900

    hello module greeings pkg

--- siteMapBuilder/go.mod
+++ siteMapBuilder/go.mod
@@ -1,5 +1,3 @@
 module github.com/Eric-lab-star/go-exercise/siteMapBuilder
 
 go 1.19
-
-require golang.org/x/net v0.5.0 // indirect
---
+++ siteMapBuilder/siteMap.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Eric-lab-star/go-exercise/hello/greetings"
+)
+
+func main() {
+	fmt.Println("site")
+	greetings.Hello()
+}
