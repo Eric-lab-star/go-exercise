ad531aaf9ac037f2e00eca6526fb7f6970b2ec26
eric-lab-star <cyon256@icloud.com>
2023-01-25 19:50:20 +0900

    postgresql setup

--- phoneNumber/main.go
+++ phoneNumber/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"regexp"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	host   = "localhost"
+	port   = 5432
+	dbname = "gophercises_phone"
 )
 
 func main() {
-	fmt.Print("hello")
+	psqlInfo := fmt.Sprintf("host=%s port=%d sslmode=disable", host, port)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = createDB(db, dbname)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
 
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func normalize(phone string) string {
