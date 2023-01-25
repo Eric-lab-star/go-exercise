fc1e7ca98c5696bd60f84d2bf935d86b7b9a2dd0
eric-lab-star <cyon256@icloud.com>
2023-01-25 17:04:31 +0900

    normalize phone number

--- phoneNumber/main.go
+++ phoneNumber/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"regexp"
 )
 
 func main() {
@@ -11,11 +11,7 @@ func main() {
 }
 
 func normalize(phone string) string {
-	var buf bytes.Buffer
-	for _, ch := range phone {
-		if ch >= '0' && ch <= '9' {
-			buf.WriteRune(ch)
-		}
-	}
-	return buf.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
+
 }
