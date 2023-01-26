f396d769f87722b6df71145392a52bfe535e363f
eric-lab-star <cyon256@icloud.com>
2023-01-26 12:56:11 +0900

    get all data from table

--- phoneNumber/main.go
+++ phoneNumber/main.go
@@ -15,6 +15,18 @@ var (
 	dbname = "gophercises_phone"
 )
 
+/*
+numbers in phone_numbers table
+1234567890
+123 456 7891
+(123) 456 7892
+(123) 456-7893
+123-456-7894
+123-456-7890
+1234567892
+(123)456-7892
+*/
+
 func main() {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d database=%s sslmode=disable", host, port, dbname)
@@ -23,29 +35,73 @@ func main() {
 	must(err)
 	defer db.Close()
 	createPhoneNumbersTable(db)
-	id, err := insertPhonNumbers(db)
+	phoneNumbers, err := getAllPhoneNumbers(db)
+
 	must(err)
-	fmt.Println("id: ", id)
+
+	for _, phoneNumber := range phoneNumbers {
+
+		fmt.Printf("id: %d, phone number: %s\n", phoneNumber.id, normalize(phoneNumber.value))
+	}
+
 }
 
 func must(err error) {
 	if err != nil {
-		fmt.Printf("Failed to call function \n err:%v", err)
+		fmt.Printf("Failed to call function \n err:%v\n", err)
 		os.Exit(1)
 	}
 }
 
-func insertPhonNumbers(db *sql.DB) (int, error) {
-	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
-	row := db.QueryRow(statement, "1234567890")
-	var id int
-	err := row.Scan(&id)
+type phoneNumber struct {
+	id    int
+	value string
+}
+
+func getAllPhoneNumbers(db *sql.DB) ([]phoneNumber, error) {
+	var ret []phoneNumber
+	statement := `SELECT * FROM phone_numbers`
+	rows, err := db.Query(statement)
 	if err != nil {
-		return -1, err
+		return ret, nil
 	}
-	return id, nil
+	for rows.Next() {
+		var phoneNumber phoneNumber
+		err := rows.Scan(&phoneNumber.id, &phoneNumber.value)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, phoneNumber)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func getPhoneNumbers(db *sql.DB, id int) (string, error) {
+	var value string
+	statement := `SELECT value FROM phone_numbers WHERE id=$1`
+	err := db.QueryRow(statement, id).Scan(&value)
+	if err != nil {
+		return "", err
+	}
+	return value, err
 
 }
+
+// func insertPhonNumbers(db *sql.DB, phoneNumber string) (int, error) {
+// 	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+// 	row := db.QueryRow(statement, phoneNumber)
+// 	var id int
+// 	err := row.Scan(&id)
+// 	if err != nil {
+// 		return -1, err
+// 	}
+
+// 	return id, nil
+
+// }
 
 func createPhoneNumbersTable(db *sql.DB) {
 	statement := `
