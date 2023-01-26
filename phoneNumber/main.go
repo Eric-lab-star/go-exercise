071d3524379242a21a7f12808235220d234bf82c
eric-lab-star <cyon256@icloud.com>
2023-01-26 10:20:41 +0900

    created database

--- phoneNumber/main.go
+++ phoneNumber/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"regexp"
 
 	_ "github.com/lib/pq"
@@ -15,26 +16,38 @@ var (
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d sslmode=disable", host, port)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d database=%s sslmode=disable", host, port, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	err = createDB(db, dbname)
-	if err != nil {
-		panic(err)
-	}
-	db.Close()
+
+	must(err)
+	defer db.Close()
+	must(db.Ping())
 
 }
 
-func createDB(db *sql.DB, name string) error {
-	_, err := db.Exec("CREATE DATABASE " + name)
+func must(err error) {
 	if err != nil {
-		return err
+		fmt.Printf("Failed to call function \n err:%v", err)
+		os.Exit(1)
 	}
-	return nil
 }
+
+// func resetDB(db *sql.DB, name string) error {
+// 	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return createDB(db, name)
+// }
+
+// func createDB(db *sql.DB, name string) error {
+// 	_, err := db.Exec("CREATE DATABASE " + name)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
 func normalize(phone string) string {
 	re := regexp.MustCompile("[^0-9]")
