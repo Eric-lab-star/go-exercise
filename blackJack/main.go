e9af8402a8221709a6e4f6b405013938150b299b
eric-lab-star <cyon256@icloud.com>
2023-02-05 18:50:57 +0900

    game state

--- blackJack/main.go
+++ blackJack/main.go
@@ -17,6 +17,38 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
+func (h Hand) DealerString() string {
+	return h[0].String() + ", **HIDEEN**"
+}
+
+func (h Hand) MinScore() int {
+	score := 0
+	for _, card := range h {
+		score += min(int(card.Rank), 10)
+	}
+	return score
+}
+
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore > 11 {
+		return minScore
+	}
+	for _, card := range h {
+		if card.Rank == deck.Ace {
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
@@ -27,11 +59,50 @@ func main() {
 			*hand = append(*hand, card)
 		}
 	}
-	fmt.Println("Player: ", player)
-	fmt.Println("dealer: ", dealer)
 
+	var input string
+	for input != "s" {
+		fmt.Println("Player:", player)
+		fmt.Println("dealer: ", dealer.DealerString())
+		fmt.Println("What will you do? (h)it, (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
+
+	pScore, dScore := player.Score(), dealer.Score()
+	fmt.Println("===Final Hands===")
+	fmt.Println("Player:", player, "\nScore:", pScore)
+	fmt.Println("dealer: ", dealer, "\nScore:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted")
+	case dScore > 21:
+		fmt.Println("You busted")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case dScore > pScore:
+		fmt.Println("You loose")
+	case dScore == pScore:
+		fmt.Println("Draw ")
+	}
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
+}
+
+type GameState struct {
+	Deck   []deck.Card
+	Turn   int
+	Player Hand
+	Dealer Hand
 }
